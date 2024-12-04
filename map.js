6b97932edf7599f4df77719682de9f4ac5beab86
giselle <giselle.mingue@gmail.com>
2024-12-03 20:46:40 -0500

    add holiday season info

--- map.js
+++ map.js
@@ -12,3 +12,32 @@ var marker = L.marker([45.32737114631411, -75.74472431487743]).addTo(map);
 marker
   .bindPopup("<b>Nepean Sportsplex</b></b><br>31701 Woodroffe Ave, Ottawa ON")
   .openPopup();
+
+window.onload = function () {
+  const modal = document.getElementById("promo-modal");
+  const closeBtn = document.querySelector(".close");
+  const viewScheduleBtn = document.getElementById("holiday-schedule");
+
+  // Display the modal on page load
+  modal.style.display = "block";
+
+  // Close the modal when clicking the close button
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // Close the modal when clicking outside of the modal content
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  // Scroll to the schedule section when clicking the button
+  viewScheduleBtn.onclick = function () {
+    document
+      .getElementById("holiday-schedule-container")
+      .scrollIntoView({ behavior: "smooth" });
+    modal.style.display = "none";
+  };
+};
